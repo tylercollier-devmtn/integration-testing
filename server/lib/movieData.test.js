a19deebc6af7d109dde840ac7c97236bedb27ee7
Tyler Collier <git@tylercollier.com>
2018-10-07 14:57:06 -0700

    Add integration test to use database

--- server/lib/movieData.test.js
+++ server/lib/movieData.test.js
@@ -1,10 +1,11 @@
 const movieData = require('./movieData');
 const sinon = require('sinon');
+const massive = require('massive');
 
 describe('unit tests', () => {
   describe('create', () => {
     it('automatically passes in the current createdAt timestamp', () => {
-      const movieName = 'Some New Movie';
+      const movieName = 'Test Movie Name';
       const fakeDb = {
         query: sinon.mock()
           .withArgs(
@@ -16,6 +17,39 @@ describe('unit tests', () => {
           )
       };
       return movieData.create(fakeDb, { name: movieName })
+    });
+  });
+});
+
+describe('integration tests', () => {
+  let db;
+  function clearDatabase() {
+    return db.query('DELETE FROM movies');
+  }
+
+  beforeAll(() => {
+    return massive(process.env.CONNECTION_STRING).then(database => {
+      db = database;
+    });
+  });
+
+  beforeEach(() => {
+    return clearDatabase();
+  });
+
+  describe('create', () => {
+    it.only('inserts a record into the database with a created_at date', () => {
+      const movieName = 'Test Movie Name';
+      return movieData.create(db, { name: movieName })
+      .then(() => {
+        return db.query('SELECT * FROM movies').then(movies => {
+          expect(movies.length).toEqual(1);
+          expect(movies[0]).toMatchObject({
+            name: movieName,
+            created_at: expect.any(Date)
+          });
+        });
+      });
     });
   });
 });
