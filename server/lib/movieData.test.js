41a136f47b6a06d0d8687846afe7ebe25fce7781
Tyler Collier <git@tylercollier.com>
2018-10-07 12:43:40 -0700

    Add sinon and unit test

--- server/lib/movieData.test.js
+++ server/lib/movieData.test.js
@@ -1,9 +1,21 @@
 const movieData = require('./movieData');
+const sinon = require('sinon');
 
 describe('unit tests', () => {
   describe('create', () => {
     it('automatically passes in the current createdAt timestamp', () => {
-      
+      const movieName = 'Some New Movie';
+      const fakeDb = {
+        query: sinon.mock()
+          .withArgs(
+            sinon.match.string,
+            sinon.match({
+              name: movieName,
+              createdAt: sinon.match.date
+            })
+          )
+      };
+      return movieData.create(fakeDb, { name: movieName })
     });
   });
 });
